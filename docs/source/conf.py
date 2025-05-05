b7556a001257f083bb9d79dd52f1f85b20325084
jguski <jannis.guski@scai.fraunhofer.de>
2025-05-05 14:04:47 +0200

    Docs: switch to os.path.abspath in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 import sys
 
-sys.path.insert(0, "../../pytmle")
+sys.path.insert(0, os.path.abspath("../.."))
 autodoc_mock_imports = [
     "pandas",
     "dataclasses",
