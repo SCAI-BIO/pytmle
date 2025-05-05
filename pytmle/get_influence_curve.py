Fix: enhance overflow handling in get_ic function

pytmle/get_influence_curve.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pytmle.estimates import UpdatedEstimates
 from pytmle.g_computation import get_g_comp
 
+
 def get_eic(
     estimates: Dict[int, UpdatedEstimates],
     event_times: np.ndarray,
@@ -156,8 +157,8 @@ def get_ic(
                 - np.mean(f_j_t[:, tau_idx])
             )
 
-            # Check for overflow (NaNs)
-            if np.any(np.isnan(ic_j_tau)):
+            # Check for overflow
+            if np.any(np.isnan(ic_j_tau)) or np.any(np.isinf(ic_j_tau)):
                 raise RuntimeError(
                     "IC overflow: either increase min_nuisance or specify a target estimand "
                     "(Target Event, Target Time, & Intervention) with more support in the data."